cd1a76dfd83cda48d48e504d527d09bc6de81bcd
sourav-kundu443 <kundusourav443@gmail.com>
2023-05-04 23:49:34 +0530

    initial

--- src/index.js
+++ src/index.js
@@ -1,23 +1,5 @@
-function getMaxSumOfSubArray(nums) {
-  // let maxSum = nums[0];
-  // for (let i = 0; i < nums.length; i++) {
-  //   let currentSum = 0;
-  //   for (let j = i; j < nums.length; j++) {
-  //     currentSum = currentSum + nums[j];
-  //     if (currentSum > maxSum) maxSum = currentSum;
-  //   }
-  // }
-  // return maxSum;
+const isPalindrome = function (value) {
+  return value.toString() === value.toString().split("").reverse().join("");
+};
 
-  // Kadane's algo
-  let currentSum = 0;
-  let maxSum = nums[0];
-  for (let i = 0; i < nums.length; i++) {
-    currentSum += nums[i];
-    if (maxSum < currentSum) maxSum = currentSum;
-    if (currentSum < 0) currentSum = 0;
-  }
-  return maxSum;
-}
-
-console.log(getMaxSumOfSubArray([-2, -1, 1]));
+console.log(isPalindrome("MADAAM"));
